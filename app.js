d7ccb4632d216db0aeca718e9814dc294b546c8b
Alice Cutler <acutler0451@gmail.com>
2021-12-28 18:46:18 -0500

    12/28 commit

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const APIKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // <-- DELETE BEFORE PUSHING TO REPO
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const APIKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // <-- ****************DELETE BEFORE PUSHING TO REPO********************
 
 app.get('/', function (req, res) {
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=Kalamazoo&appid=${APIKey}`;
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+	const city = req.body.cityName;
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
 	https.get(url, function (response) {
 		console.log(response.statusCode);
@@ -22,7 +30,7 @@ app.get('/', function (req, res) {
 			const icon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
 			res.write(`<p>The weather is currently ${weatherDescription}.</p>`);
 			res.write(
-				`<h1>The temperature in Kalamazoo is ${temp} degrees fahrenheit.</h1>`
+				`<h1>The temperature in ${city} is ${temp} degrees fahrenheit.</h1>`
 			);
 			res.write(`<img src="${icon}">`);
 			res.send();
